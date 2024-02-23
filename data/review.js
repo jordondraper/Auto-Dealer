42d5251900f56deca6d2f785d340afe6b3800587
Jordon Draper <jordondraper17@gmail.com>
2024-02-22 21:40:54 -0700

    upload 1

---
+++ data/review.js
@@ -0,0 +1,27 @@
+export const review = [
+    {
+      "name": "Paul Cheney",
+      "stars": 1,
+      "text": "They didn't have any Mustangs so I didn't like it that much."
+    },
+    {
+      "name": "Emily Hedrick",
+      "stars": 4,
+      "text": "I like having the Car Cam feature for the cars because I really enjoy cameras."
+    },
+    {
+      "name": "Dan Hatch",
+      "stars": 3,
+      "text": "It was okay. It wasn't Designy enough."
+    },
+    {
+      "name": "Mike Harper",
+      "stars": 2,
+      "text": "Could have had better features like Tesla."
+    },
+    {
+      "name": "Thor Anderson",
+      "stars": 5,
+      "text": "Thor."
+    }
+  ]
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,59 @@
+// toggle menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("xButton").classList.toggle("open");
+}
+
+const x = document.getElementById("xButton");
+
+x.onclick = toggleMenu;
+
+
+
+// review from data file
+import { review } from '../data/review.js';
+console.log(review);
+
+// empty card
+
+const myTarget = document.querySelector("#cards");
+
+for (let x = 0; x < review.length; x++) {
+
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = review[x].name
+    
+    const myReview = document.createElement('p')
+    myReview.textContent = review[x].text
+
+    const myRating = document.createElement('div')
+
+    
+
+console.log(review[x].stars)
+
+for (let step = 0; step<review[x].stars; step++) {
+    const revIndicator = document.createElement('img')
+    revIndicator.src = "../svg/star-outline.svg"
+    myRating.appendChild(revIndicator)
+}
+
+    //figure with child element
+    mySection.appendChild(myName);
+    mySection.appendChild(myRating);
+    mySection.appendChild(myReview);
+
+    myTarget.appendChild(mySection);
+}// end of loop
+
+
+
+
+
+
+
+
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
